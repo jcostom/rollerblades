24f04ad51613e95a33e67940e633e96a2f916c44
Jason Costomiris <jcostom@jasons.org>
2023-02-22 16:21:35 -0500

    ready to merge v0.8

--- rollerblades.py
+++ rollerblades.py
@@ -22,7 +22,7 @@ USE_MONTHS = int(os.getenv('USE_MONTHS', 1))
 DEBUG = int(os.getenv('DEBUG', 0))
 
 # --- Globals ---
-VER = '0.8-dev'
+VER = '0.8'
 USER_AGENT = f"rollerblades.py/{VER}"
 KEY = 'CinemaTrailersPrerollID'
 
